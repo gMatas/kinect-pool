4560ffb2c55302f5a1657e12bcb32a146a11c61f
Matthew <26392564+gMatas@users.noreply.github.com>
2021-05-17 23:58:06 +0300

    Add point projection between near points and and distance between them ratio

--- Kinect Pool/Assets/Scripts/FittedPoint.cs
+++ Kinect Pool/Assets/Scripts/FittedPoint.cs
@@ -7,8 +7,15 @@ public class FittedPoint : MonoBehaviour
 {
     public FittedPlane Plane;
     public Transform PointProjection;
+    public Transform PointProjectionOnNearPoints;
+
+    public Transform A;
+    public Transform B;
+    public Transform C;
 
     private Vector3 _pointProjection;
+    private Vector3 _pointNearProjection;
+    private float _pointNearProjectionDistanceRatio;
 
     public bool IsWithinPlane()
     {
@@ -20,25 +27,52 @@ public class FittedPoint : MonoBehaviour
         return false;
     }
 
+    public float GetPointNearProjectionDistanceRatio()
+    {
+        return _pointNearProjectionDistanceRatio;
+    }
+
+    public Vector3 GetPointNearProjection()
+    {
+        return _pointNearProjection;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         FitPointOnPlane();
+        ComputePointNearProjection();
     }
 
     // Update is called once per frame
     void Update()
     {
         FitPointOnPlane();
+        ComputePointNearProjection();
 
         if (Application.isEditor)
         {
             if (PointProjection) PointProjection.position = _pointProjection;
-        }
+            if (PointProjectionOnNearPoints) PointProjectionOnNearPoints.position = _pointNearProjection;
+        }        
     }
 
     void FitPointOnPlane()
     {
         _pointProjection = Plane.FitPoint(transform.position);
+    }
+
+    void ComputePointNearProjection()
+    {
+        Vector3 basePoint = Plane.CloseLeftPoint.position;
+        Vector3 a = Plane.CloseRightPoint.position - basePoint;
+        Vector3 b = _pointProjection - basePoint;
+
+        float projectionMagnitude = b.magnitude * Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(b, a));
+        _pointNearProjection = projectionMagnitude * a.normalized + basePoint;
+
+        float maxDistance = Vector3.Distance(Plane.CloseLeftPoint.position, Plane.CloseRightPoint.position);
+        float distance = Vector3.Distance(_pointNearProjection, Plane.CloseRightPoint.position);
+        _pointNearProjectionDistanceRatio = (maxDistance - distance) / maxDistance;
     }
 }
